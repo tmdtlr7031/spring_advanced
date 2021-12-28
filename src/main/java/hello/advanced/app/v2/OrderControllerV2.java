7ee6783b05c3d0e2d3b4eba37a3e2254488dc1aa
namseungsik <tmd7031@naver.com>
2021-12-28 22:14:24 +0900

    로그추적기 V2 - 적용

---
+++ src/main/java/hello/advanced/app/v2/OrderControllerV2.java
@@ -0,0 +1,30 @@
+package hello.advanced.app.v2;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV2;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+    private final HelloTraceV2 trace;
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderControllerV1.request()");
+            orderService.orderItem(status.getTraceId(), itemId);
+            trace.end(status);
+            return "ok";
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 로그가 로직에 영향을 주면 안되기 때문에 다시 던짐.
+        }
+    }
+}
